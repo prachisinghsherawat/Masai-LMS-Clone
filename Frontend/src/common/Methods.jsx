ebae4ab942722cb6b79d3a8a095b197a42c77cbc
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-09-02 23:21:53 +0530

    methods updated

--- Frontend/src/common/Methods.jsx
+++ Frontend/src/common/Methods.jsx
@@ -1,29 +1,30 @@
 
 import axios from "axios"
 
-const baseUrl = "http://localhost:8080";
+export const baseUrl = "http://localhost:8080";
 
 
 
-export const postApi = (baseUrl,paramDetails,payload)=>{ 
+
+export const postApi = (baseUrl,paramDetails,payload) =>{ 
 
     axios.post(baseUrl+paramDetails,payload)
 
 }
 
-export const getApi = (baseUrl,paramDetails)=>{ 
+export const getApi = (baseUrl,paramDetails) =>{ 
 
     axios.get(baseUrl+paramDetails)
 
 }
 
-export const updateApi = (baseUrl,paramDetails,payload)=>{ 
+export const updateApi = (baseUrl,paramDetails,payload) =>{ 
 
     axios.patch(baseUrl+paramDetails,payload)
 
 }
 
-export const deleteApi = (baseUrl,paramDetails)=>{ 
+export const deleteApi = (baseUrl,paramDetails) =>{ 
 
     axios.delete(baseUrl+paramDetails)
 

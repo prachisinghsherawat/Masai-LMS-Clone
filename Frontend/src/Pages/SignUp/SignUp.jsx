5afd40040782ea2a9c075e3606b26d5f6dfac72d
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-09-02 20:10:05 +0530

    navbar

---
+++ Frontend/src/Pages/SignUp/SignUp.jsx
@@ -0,0 +1,59 @@
+
+
+
+export const SignUp = ()=>{
+
+   
+
+
+    return(
+
+
+        <div>
+            <Grid container spacing={2}>
+           <div>
+           <Grid item xs={8}>
+            <TextField
+                 id="outlined-name"
+                 label="Name"
+                 value={name}
+                 onChange={handleChange}
+                />
+            </Grid>
+           </div>
+            <div>
+            <Grid item xs={8}>
+            <TextField
+                 id="outlined-name"
+                 label="Name"
+                 value={name}
+                 onChange={handleChange}
+                />
+            </Grid>
+            </div>
+             <div>
+             <Grid item xs={8}>
+             <TextField
+                 id="outlined-name"
+                 label="Name"
+                 value={name}
+                 onChange={handleChange}
+                />
+             </Grid>
+             </div>
+             <div>
+             <Grid item xs={8}>
+             <TextField
+                 id="outlined-name"
+                 label="Name"
+                 value={name}
+                 onChange={handleChange}
+                />
+             </Grid>
+             </div>
+           </Grid>
+        </div>
+    )
+
+}
+

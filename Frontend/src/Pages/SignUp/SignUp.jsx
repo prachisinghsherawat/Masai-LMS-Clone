ff56a8f15ceaacbc629acc217e10a6c1c9359c40
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-09-03 13:47:19 +0530

    signup updated

--- Frontend/src/Pages/SignUp/SignUp.jsx
+++ Frontend/src/Pages/SignUp/SignUp.jsx
@@ -2,11 +2,46 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import SimpleReactValidator from 'simple-react-validator';
 import "./SignUp.css"
+import { useState } from 'react';
 
 
 
 export const SignUp = () => {
+
+  const simpleValidator = React.useRef(new SimpleReactValidator())
+
+  const [signupData , setSignupData] = useState({
+
+    name : "",
+    username : "",
+    email : "",
+    password : ""
+
+  })
+
+  const [ ,forceUpdate] = useState()
+
+  
+
+  const HandleChange = (e) => {
+
+    const {name , value} = e.target;
+    setSignupData({...signupData , [name]:value })
+  }
+
+  const HandleSubmit = () => {
+
+    if(simpleValidator.current.allValid()){
+      console.log("yes")
+    }
+    else{
+      simpleValidator.current.showMessages()
+      forceUpdate(1)
+    }
+  }
+
 
   return (
 
@@ -17,12 +52,13 @@ export const SignUp = () => {
         <div>
           <Box
             sx={{
-               width: 500,
-               maxWidth: '100%',
+              width: 500,
+              maxWidth: '100%',
             }}
           >
             <label>Name</label>
-            <TextField fullWidth id="fullWidth" />
+            <TextField fullWidth id="fullWidth" name='name' onChange={HandleChange} />
+            <span>{simpleValidator.current.message("name" , signupData?.name , "required")}</span>
 
           </Box>
         </div>
@@ -30,12 +66,13 @@ export const SignUp = () => {
         <div>
           <Box
             sx={{
-               width: 500,
-               maxWidth: '100%',
+              width: 500,
+              maxWidth: '100%',
             }}
           >
             <label>Username</label>
-            <TextField fullWidth id="fullWidth" />
+            <TextField fullWidth id="fullWidth" name='username' onChange={HandleChange} />
+            <span>{simpleValidator.current.message("username" , signupData?.username , "required")}</span>
 
           </Box>
         </div>
@@ -43,12 +80,13 @@ export const SignUp = () => {
         <div>
           <Box
             sx={{
-               width: 500,
-               maxWidth: '100%',
+              width: 500,
+              maxWidth: '100%',
             }}
           >
             <label>Email</label>
-            <TextField fullWidth id="fullWidth" />
+            <TextField fullWidth id="fullWidth" name='email' onChange={HandleChange} />
+            <span>{simpleValidator.current.message("email" , signupData?.email , "required")}</span>
 
           </Box>
         </div>
@@ -56,19 +94,20 @@ export const SignUp = () => {
         <div>
           <Box
             sx={{
-               width: 500,
-               maxWidth: '100%',
+              width: 500,
+              maxWidth: '100%',
             }}
           >
             <label>Password</label>
-            <TextField fullWidth id="fullWidth" />
+            <TextField fullWidth id="fullWidth" name='password' onChange={HandleChange} />
+            <span>{simpleValidator.current.message("password" , signupData?.password , "required")}</span>
 
           </Box>
         </div>   
 
         <div>
     
-          <Button className='btn' variant="contained">SIGN UP</Button>
+          <Button onClick={HandleSubmit} className='btn' variant="contained">SIGN UP</Button>
             
         </div>
 
